c24c9d3be1f37a508480958fc35bcb0b5dc91c21
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-25 17:28:04 -0300

    Implementado Menu Solicitando input do Usuário e validação dos valores de entrada.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Diamante_de_X
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Diamante_de_X
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             
+            //Entrada do número para criação do triângulo e validação
+            while (true)
+            {
+
+                Console.WriteLine("---------------------------------------");
+                Console.Write("Insira um valor ímpar para realizar o desenho do diamante: ");
+                string entradaNumeroUsuario = Console.ReadLine();
+                int numeroDeX;
+
+                if (!int.TryParse(entradaNumeroUsuario, out numeroDeX))
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Por favor, utilize apenas números.");
+                    continue;
+                }              
+                                
+                if (numeroDeX % 2 == 0)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Valor inserido é par! Insira um número novamente: ");
+                    continue;
+                }
+                else                    
+                break;
+
+            }
+            
+            Console.ReadLine();
         }
     }
 }
