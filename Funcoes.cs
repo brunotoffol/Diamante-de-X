9b04527c26252ed4eba1803b81b9786173f3d8ef
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-25 19:10:59 -0300

    Refatorado o código para utilizar classes e métodos.

---
+++ Funcoes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamante_de_X
+{
+    class Funcoes
+    {
+
+        //Metodo para Exibir o Menu
+        static public int ExibirMenu()
+        {
+            int numeroDeX;
+            while (true)
+            {
+
+                Console.WriteLine("---------------------------------------");
+                Console.Write("Insira um valor ímpar para realizar o desenho do diamante: ");
+                string entradaNumeroUsuario = Console.ReadLine();
+
+
+                if (!int.TryParse(entradaNumeroUsuario, out numeroDeX))
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Por favor, utilize apenas números.");
+                    continue;
+                }
+
+                else if (numeroDeX % 2 == 0)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Valor inserido é par! Insira um número novamente: ");
+                    continue;
+                }
+                else
+                    return numeroDeX;
+
+            }
+        }
+       
+        //Loops que realizam o desenho do diamante de X
+        static public void DesenhoDiamante(int numeroDeX)
+        {
+            for (int i = 1; i <= numeroDeX; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = numeroDeX; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Diamante_de_X
@@ -8,54 +9,15 @@ namespace Diamante_de_X
         static void Main(string[] args)
         {
 
-            int numeroDeX;            
             //Entrada do número para criação do triângulo e validação
-            while (true)
-            {
+            int numeroDeX = Funcoes.ExibirMenu();
 
-                Console.WriteLine("---------------------------------------");
-                Console.Write("Insira um valor ímpar para realizar o desenho do diamante: ");
-                string entradaNumeroUsuario = Console.ReadLine();                
-
-                if (!int.TryParse(entradaNumeroUsuario, out numeroDeX))
-                {
-                    Console.WriteLine("---------------------------------------");
-                    Console.WriteLine("Por favor, utilize apenas números.");
-                    continue;
-                }              
-                                
-                else if (numeroDeX % 2 == 0)
-                {
-                    Console.WriteLine("---------------------------------------");
-                    Console.WriteLine("Valor inserido é par! Insira um número novamente: ");
-                    continue;
-                }
-                else                    
-                break;
-
-            }
-
-            //Laços de repetição que desenham o Losango  
-            for (int i = 1; i <= numeroDeX; i++)
-            {                   
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("X");
-                }
-                Console.WriteLine();
-            }
-
-            for (int i = numeroDeX; i >= 1; i--)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("X");
-                }
-                Console.WriteLine();
-            }
-
+            //Laços de repetição que fazem o desenho  
+            Funcoes.DesenhoDiamante(numeroDeX);
 
             Console.ReadLine();
+
+            
         }
     }
 }
